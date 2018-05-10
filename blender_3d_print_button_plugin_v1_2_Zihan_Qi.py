275e53cd0ae63ac8ff79133605a328bb48915c96
MuteBardTison <z.qi@jacobs-university.de>
2018-05-10 18:02:52 +0200

    upload

--- blender_3d_print_button_plugin_v1_2_Zihan_Qi.py
+++ blender_3d_print_button_plugin_v1_2_Zihan_Qi.py
@@ -22,19 +22,25 @@ class PrintButtonPanel(bpy.types.Panel):
     bl_category = "Tools"
     bl_label = "3D Print Button"
 
-    bpy.types.Scene.my_bool = bpy.props.BoolProperty(
-        name = "True/False",
-        description="A bool property",
-        default = False
-    )
+    bpy.types.Scene.options_enum = bpy.props.EnumProperty(
+        items = [('Together', 'Together', ''),
+                 ('Independently', 'Independently', '')],
+        name = "Upload")
+
+    #bpy.types.Scene.options_bool = bpy.props.BoolProperty(
+        #name = "Upload Independently",
+        #description="Upload Independently",
+        #default = False
+    #)
+
     def draw(self, context):
-        # Checkbox for 
-        self.layout.prop(context.scene, "my_bool")
+        # Checkbox for upload options
+        #self.layout.prop(bpy.context.scene, "options_bool")
+        # Drop down List for upload options
+        self.layout.prop(bpy.context.scene, "options_enum")
         # Button
         self.layout.operator("object.export_stl", text="3D Print Button")
-
-
-        
+  
 class ExportToStl(bpy.types.Operator):
     bl_idname = "object.export_stl"
     bl_label = "Send to 3D-button.com"
@@ -61,9 +67,10 @@ class ExportToStl(bpy.types.Operator):
                 
             # Selected Objects
             if(len(bpy.context.selected_objects)>0):
-                files = {}
+                files = []
                 open_files = []
-                if(len(bpy.context.selected_objects) == 1):
+                #if(len(bpy.data.objects) == 1 or (len(bpy.data.objects) != 1 and bpy.context.scene.options_bool == False)):
+                if(len(bpy.data.objects) == 1 or (len(bpy.data.objects) != 1 and bpy.context.scene.options_enum == 'Together')):
                     # Save as .stl file. Because the blender method export_mesh.stl needs a new file, 
                     # a temporary file is created in the (roaming) blender directory
                     # .stl file is in ascii, due to request problems
@@ -72,6 +79,7 @@ class ExportToStl(bpy.types.Operator):
                     bpy.ops.export_mesh.stl(filepath=path, ascii=True) 
                     
                     file = open(path)
+                    
                     open_files.append(file)
 
                     if bpy.data.is_saved == False:
@@ -80,13 +88,13 @@ class ExportToStl(bpy.types.Operator):
                         # get filename
                         somename = bpy.path.basename(bpy.context.blend_data.filepath)
                         filename=somename[:-6]
+                    files.append(("file", (filename + ".stl", file, 'application/sla')))
+                    #files = {'file':(filename + ".stl", file,'application/sla')}
 
-                    files = {'file':(filename + ".stl", file,'application/sla')}
-                ###############
                 else:
-                    path = os.path.dirname(os.path.abspath(__file__))
-                    bpy.ops.export_mesh.stl(filepath=path, ascii=True, batch_mode = 'OBJECT') 
-                    
+                    path = os.path.dirname(os.path.abspath(__file__)) + "/"
+                    bpy.ops.export_mesh.stl(filepath=path, ascii=True, batch_mode='OBJECT') 
+
                     if bpy.data.is_saved == False:
                         filename='unsaved_file'
                     else:
@@ -96,57 +104,59 @@ class ExportToStl(bpy.types.Operator):
 
                     index = 0
                     for stl_file in os.listdir(path):
-                        if stl_file.endswith(".stl"):
-                            file = open(path + "/" + stl_file)
+                        if stl_file.endswith(".stl") and (not stl_file.lower().startswith(("camera", "lamp"))):
+                            file = open(path + stl_file)
                             open_files.append(file)
-                            files['file' + str(index)] = (filename + str(index) + ".stl", file, 'application/sla')
+                            files.append(("file", (filename + str(index) + ".stl", file, 'application/sla')))
+                            #files['file' + str(index)] = (filename + str(index) + ".stl", file, 'application/sla')
                             index += 1
+                    index = 0
 
-                ##############
                 #HTTP POST request, using the python REQUEST lib
                 url = 'https://api.3d-button.com/upload'
                 fields = { "origin":"blender_1_1"}
                 custom_header = {
                     "Accept":"text/plain"
                     }
-                try:
-                    r = requests.post(url, files=files, data=fields,headers=custom_header,verify=False)
-                    # Open the returned URL
-                    if sys.platform[:3]== 'win':
-                        webbrowser.get('windows-default').open(r.text) 
-                   # for browser in (    
-                    #                    'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s',
-                    #                    'C:/Program Files (x86)/Mozilla Firefox/firefox.exe %s',
-                    #                    'C:/Program Files/Internet Explorer/iexplore.exe %s'):
-                    #    try:
-                    #        b=webbrowser.get(browser)
-                    #        b.open(r.text)
-                    #        break
-                    #    except:
-                    #        pass
-             
-                    else:
-                        webbrowser.open(r.text)
-                    # Show the url
-                    self.report({'INFO'},r.text)
+                for f in files:
+                    try:
+                        r = requests.post(url, files={f[0]:f[1]}, data=fields, headers=custom_header, verify=False)
+                        # Open the returned URL
+                        if sys.platform[:3]== 'win':
+                            webbrowser.get('windows-default').open(r.text) 
+                    # for browser in (    
+                        #                    'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s',
+                        #                    'C:/Program Files (x86)/Mozilla Firefox/firefox.exe %s',
+                        #                    'C:/Program Files/Internet Explorer/iexplore.exe %s'):
+                        #    try:
+                        #        b=webbrowser.get(browser)
+                        #        b.open(r.text)
+                        #        break
+                        #    except:
+                        #        pass
                 
-                    # Deselect
-                    bpy.ops.object.select_all(action='DESELECT')
-                        
+                        else:
+                            webbrowser.open(r.text)
+                        # Show the url
+                        self.report({'INFO'},r.text)
                     
-                except:
-                    #Error Message in info console
-                    self.report({'INFO'},'Could not connect to server. Please check your internet connection.')
+                        # Deselect
+                        bpy.ops.object.select_all(action='DESELECT')
+                            
+                        
+                    except:
+                        # Error Message in info console
+                        self.report({'INFO'},'Could not connect to server. Please check your internet connection.')
 
-                # Remove temp file
+                # Close opeing files
                 map(lambda f: f.close(), open_files)
-
-                #for p in os.listdir(os.path.dirname(os.path.abspath(__file__))):
-                #    if p.endswith(".stl"):   
-                #        os.remove(os.path.dirname(os.path.abspath(__file__)) + "/" + p)
-                      
+                # Remove temp file
+                for p in os.listdir(os.path.dirname(os.path.abspath(__file__))):
+                    if p.endswith(".stl"):   
+                            os.remove(os.path.dirname(os.path.abspath(__file__)) + "/" + p)
+                    
                 return {'FINISHED'}
-                
+                    
             else:
                 #Error Message in info console
                 self.report({'INFO'},'Select an object')
